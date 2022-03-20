394d82f9a6e4e1bd41a637cc63c0570f13af85d8
Kausalya.K <85003124+kausalya17897@users.noreply.github.com>
2022-03-20 11:38:05 +0530

    init

---
+++ services/employee.service.js
@@ -0,0 +1,57 @@
+import db from '../mongo.js';
+import { ObjectId } from 'mongodb';
+import { request } from 'express';
+const service={
+   async getEmployee(request,response){
+        try{
+            const data=await db.employee.find().toArray();
+            console.log("employee fetched")
+            response.json(data);
+                }catch(err){
+                    console.log(" error in employee fetching")
+                    response.send(err)
+                }
+    },
+    async addEmployee(request,response){
+        try{
+            const {insertedId:_id}=await db.employee
+            .insertOne(request.body);
+            console.log("employee inserted",_id,request.body)
+            response.json({status:"success",data:{_id, ...request.body}});
+            console.log("employee added")
+                }catch(err){
+                    console.log("error in employee inserted")
+                    response.send(err);
+                }
+    },
+    async updateEmployee(request,response){
+        try{
+
+            //validation params and body
+            //validation indb
+            const {value}=await db.employee
+            .findOneAndUpdate(
+                {_id:ObjectId(request.params.id)},
+                {$set:{...request.body}});
+                //{returnDocument:"after"});
+               //new version mongodb {returnNewDocument:"true"}
+               console.log("employee edited",request.params.id,request.body)
+            response.json({status:"success",data:value});
+                }catch(err){
+                    console.log("error in employee edited")
+                    response.json({status:"error",error:"Cannot update"});
+                }
+    },
+    async deleteEmployee(request,response){
+        try{
+            await db.employee
+            .deleteOne({_id:ObjectId(request.params.id)});
+            console.log("employee deleted",request.params.id)
+            response.json({status:"success"})
+        }catch(err){
+            console.log("error in employee deleted")
+            response.json({status:"err",error:"cannot delete"})
+        }
+    }
+}
+export default service;
